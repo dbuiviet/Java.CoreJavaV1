6d49979c7473b88a623095918b63e27728ffa904
dbuiviet <dbuiviet@gmail.com>
2019-05-22 14:35:02 +0700

    34NKT

--- com/dbuiviet/java/CoreJavaV1/Employee.java
+++ com/dbuiviet/java/CoreJavaV1/Employee.java
@@ -1,6 +1,7 @@
 package com.dbuiviet.java.CoreJavaV1;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 // import statements come after the package statement
 
@@ -40,5 +41,34 @@ public class Employee
    {
       double raise = salary * byPercent / 100;
       salary += raise;
+   }
+
+   public boolean equals(Object otherObject)
+   {
+      // a quick test to see if the objects are identical
+      if (this == otherObject) return true;
+
+      // must return false if the explicit parameter is null
+      if (otherObject == null) return false;
+
+      // if the classes don't match, they can't be equal
+      if (getClass() != otherObject.getClass()) return false;
+
+      // now we know otherObject is a non-null Employee
+      Employee other = (Employee) otherObject;
+
+      // test whether the fields have identical values
+      return Objects.equals(name, other.name) && salary == other.salary && Objects.equals(hireDay, other.hireDay);
+   }
+
+   public int hashCode()
+   {
+      return Objects.hash(name, salary, hireDay); 
+   }
+
+   public String toString()
+   {
+      return getClass().getName() + "[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay
+            + "]";
    }
 }
